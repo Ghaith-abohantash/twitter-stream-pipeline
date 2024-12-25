ded7618fe03fcdac1b98e6edceeb315919c042c0
mohammadAlawnah <malawnah2003@gmail.com>
2024-12-26 01:07:27 +0200

    SentimentAnalysis

--- build.sbt
+++ build.sbt
@@ -9,6 +9,11 @@ lazy val root = (project in file("."))
       "org.scala-lang" % "scala-library" % "2.12.10",
       "org.json4s" %% "json4s-native" % "4.0.3",
       "org.json4s" %% "json4s-jackson" % "4.0.3",
-"com.typesafe.play" %% "play-json" % "2.9.2"
+      "com.typesafe.play" %% "play-json" % "2.9.2",
+      "org.apache.spark" %% "spark-core" % "3.5.2",
+      "org.apache.spark" %% "spark-sql" % "3.5.2",
+      "org.apache.spark" %% "spark-mllib" % "3.5.2",
+      "com.johnsnowlabs.nlp" %% "spark-nlp" % "5.1.0",
+      "org.apache.spark" %% "spark-sql" % "3.5.2"
     )
   )
--- src/main/scala/SentimentAnalysis.scala
+++ src/main/scala/SentimentAnalysis.scala
@@ -1,3 +1,50 @@
-class SentimentAnalysis {
+import com.johnsnowlabs.nlp.base.DocumentAssembler
+import com.johnsnowlabs.nlp.annotators.Tokenizer
+import com.johnsnowlabs.nlp.annotators.Normalizer
+import com.johnsnowlabs.nlp.annotators.sda.vivekn.ViveknSentimentModel
+import com.johnsnowlabs.nlp.Finisher
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
+object SentimentAnalysis {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("NLP")
+      .master("local[*]")
+      .config("spark.local.dir", "F:/spark-temp")
+      .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.LocalFileSystem")
+      .getOrCreate()
+
+    import spark.implicits._
+
+
+   def  processTweets(rawTweets: DataFrame, spark: SparkSession): Unit = {
+    val document = new DocumentAssembler()
+      .setInputCol("tweet")
+      .setOutputCol("document")
+
+    val token = new Tokenizer()
+      .setInputCols("document")
+      .setOutputCol("token")
+
+    val normalizer = new Normalizer()
+      .setInputCols("token")
+      .setOutputCol("normal")
+
+    val vivekn = ViveknSentimentModel.pretrained()
+      .setInputCols("document", "normal")
+      .setOutputCol("result_sentiment")
+
+    val finisher = new Finisher()
+      .setInputCols("result_sentiment")
+      .setOutputCols("final_sentiment")
+
+    val pipeline = new Pipeline().setStages(Array(document, token, normalizer, vivekn, finisher))
+
+    val pipelineModel = pipeline.fit(rawTweets)
+
+    val result = pipelineModel.transform(rawTweets)
+
+    result.select("tweet", "final_sentiment").show(false)
+  }
 }
